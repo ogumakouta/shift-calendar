6b711bd75668193764d6ee5c947cf6d550ec7a55
小熊幸汰 <siw23010010@gmail.com>
2025-07-14 12:11:55 +0900

    3回目のマイグレーションを実行

---
+++ backend/prisma/migrations/20250714031137_add_user_id_unique/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[user_id]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE `User` MODIFY `tel` VARCHAR(20) NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX `User_user_id_key` ON `User`(`user_id`);
