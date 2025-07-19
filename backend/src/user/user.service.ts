3196bfc9bc1f8cdaeb7b4f09bdbc0ad918547c25
小熊幸汰 <siw23010010@gmail.com>
2025-07-19 13:16:47 +0900

    ユーザ取得と新規登録のロジックを追加 コード追加：user.service.ts

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { AuthDto } from './dto/authDto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    // ユーザ取得
+    async getUser(data: AuthDto): Promise<User | null> {
+        return await this.prisma.user.findUnique ({
+            where: {email: data.email},
+        });
+    }
+    //新規登録
+    async createSecureServer(data: AuthDto): Promise<User> {
+        const { user_id, name, email, birthday, password } = data;
+        const hashPassword = await bcrypt.hash(password, 12);
+        return await this.prisma.user.create ({
+            data: { user_id, name, email, birthday, password_hash: hashPassword },
+        });
+    }
+}
