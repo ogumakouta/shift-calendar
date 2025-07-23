d8247de92e233c167f32c4cbc006e579cf8f12d8
小熊幸汰 <siw23010010@gmail.com>
2025-07-23 21:23:44 +0900

    ユーザ新規登録のルーティングを追加 コード追加：user.controller.ts

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create_user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly usersService: UserService) {}
+
+  // ユーザー新規登録
+  @Post('/create')
+  create(@Body(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true })) createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+}
