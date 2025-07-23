35fe597c1f90278c1bceb76a8a52de11a5a67b39
小熊幸汰 <siw23010010@gmail.com>
2025-07-24 00:58:16 +0900

    h1タグを中央寄せにして、ログインフォームを追加 コード追加：login/page.tsx

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,7 +1,10 @@
+import LoginForm from "../../../components/LoginForm";
+
 export default function LoginPage() {
     return (
-      <main>
-        <h1>ログイン</h1>
-      </main>
+        <main>
+            <h1 style={{ textAlign: 'center' }}>ログイン</h1>
+            <LoginForm />
+        </main>
     );
-  }
+}
