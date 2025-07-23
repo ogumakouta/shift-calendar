47e8492eeb4a668a8ab8c99d638ef4a24cb5e43e
小熊幸汰 <siw23010010@gmail.com>
2025-07-24 00:00:15 +0900

    UserServiceをアプリ全体で使えるようにexportsに追加 コード追加：user.module.ts

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -5,7 +5,7 @@ import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [UserController],
-  // providersにUserServiceとPrismaServiceを登録
   providers: [UserService, PrismaService],
+  exports: [UserService],
 })
 export class UserModule {}
