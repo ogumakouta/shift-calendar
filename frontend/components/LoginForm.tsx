c037f98a3438ff594324493d7eeb94b020be8c34
小熊幸汰 <siw23010010@gmail.com>
2025-07-24 02:40:52 +0900

    axiosを使ってログインAPIにリクエストを送ってレスポンスに応じた処理、フォーム内にエラーメッセージを出すdevタグを追加 コード追加：LoginForm.tsx

--- frontend/components/LoginForm.tsx
+++ frontend/components/LoginForm.tsx
@@ -1,14 +1,40 @@
 'use client';
 import { useState } from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
 
 export default function LoginForm() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const router = useRouter();
+
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL
+//   console.log(apiUrl);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log('ログイン情報:', { email, password });
-    alert(`ログイン試行: ${email}`);
+    setError('');
+    axios.post(`${apiUrl}/auth/login`, {
+        email: email,
+        password: password,
+    })
+    .then(res => {
+        console.log('レスポンス：', res.data);
+        if (res.data.access_token) {
+            localStorage.setItem('access_token', res.data.access_token);
+            router.push('/calendar');
+        }
+    })
+    .catch((error: any) => {
+        if (error.response && error.response.data && error.response.data.message) {
+            setError(error.response.data.message);
+            console.log('エラー内容:', error.response.data);
+        } else {
+            setError('ログインに失敗しました');
+            console.log('その他のエラー:', error);
+        }
+    });
   };
 
   return (
@@ -16,6 +42,9 @@ export default function LoginForm() {
       onSubmit={handleSubmit} 
       className="flex flex-col gap-4 max-w-md mx-auto my-8 p-8 border rounded-lg shadow-md"
     >
+      <div className="text-center text-red-500">
+        {error}
+      </div>
       <div className="flex flex-col gap-2">
         <label htmlFor="email" className="font-semibold">メールアドレス:</label>
         <input
