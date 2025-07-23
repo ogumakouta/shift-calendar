8527be963363aaa60f51d90d1ceac307b36797a8
小熊幸汰 <siw23010010@gmail.com>
2025-07-23 23:58:46 +0900

    ログイン機能を実装 コード追加：auth.service.ts

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UserService, private jwtService: JwtService,) {}
+
+  // ユーザ認証
+  async validateUser(email: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOneByEmail(email);
+    if (user) {
+      // bcrypt.compareで平文パスワードとハッシュ値を比較
+      const isMatch = await bcrypt.compare(pass, user.password_hash);
+      if (isMatch) {
+        // パスワードが一致した場合、パスワードハッシュを除いたユーザー情報を返す
+        const { password_hash, ...result } = user;
+        return result;
+      }
+    }
+    // ユーザが見つからなかったら
+    return null;
+  }
+
+  // ログインしてJWTトークンを返す
+  async login(user: any) {
+    const payload = { email: user.email, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
