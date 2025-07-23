176a32baf6cbba680ce4c96f5f65a6666e6b1bb8
小熊幸汰 <siw23010010@gmail.com>
2025-07-23 21:02:11 +0900

    コードを整形：prisma.service.ts

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -2,15 +2,14 @@ import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService
-    extends PrismaClient
-    implements OnModuleInit, OnModuleDestroy
-{
-    async onModuleInit() {
-        await this.$connect();
-    }
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  async onModuleInit() {
+    // アプリ開始時にデータベースに接続します。
+    await this.$connect();
+  }
 
-    async onModuleDestroy() {
-        await this.$disconnect();
-    }
+  async onModuleDestroy() {
+    // アプリ終了時にデータベースから切断
+    await this.$disconnect();
+  }
 }
