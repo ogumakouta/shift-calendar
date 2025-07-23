5be27b5ad5c09ac1ad0fe1d374f9c01472267797
小熊幸汰 <siw23010010@gmail.com>
2025-07-23 22:32:36 +0900

    authに必要な機能を読み込む コード追加：auth.module.ts

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
-@Module({})
+@Module({
+  imports: [ UserModule, JwtModule.register({secret: 'YOUR_SECRET_KEY', signOptions: {expiresIn: '60m'}}) ],
+  providers: [AuthService],
+  controllers: [AuthController]
+})
 export class AuthModule {}
