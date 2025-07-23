3354b0349e1b2b6543f2ab37bfb610798f576910
小熊幸汰 <siw23010010@gmail.com>
2025-07-23 21:25:24 +0900

    userControllerを読み込む コード追加：user.module.ts

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
-// import { UserController } from './user.controller';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { UserController } from './user.controller';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
+  controllers: [UserController],
+  // providersにUserServiceとPrismaServiceを登録
   providers: [UserService, PrismaService],
-  // controllers: [UserController]
 })
 export class UserModule {}
