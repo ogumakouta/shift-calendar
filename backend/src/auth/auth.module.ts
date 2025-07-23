b6240682b7a778ba5f605d52d7b050e8451b7252
小熊幸汰 <siw23010010@gmail.com>
2025-07-23 23:57:24 +0900

    JwtModuleにglobal: trueを追加 コード追加：auth.module.ts

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -5,7 +5,7 @@ import { UserModule } from 'src/user/user.module';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [ UserModule, JwtModule.register({secret: 'YOUR_SECRET_KEY', signOptions: {expiresIn: '60m'}}) ],
+  imports: [ UserModule, JwtModule.register({global: true, secret: 'YOUR_SECRET_KEY', signOptions: {expiresIn: '60m'}}) ],
   providers: [AuthService],
   controllers: [AuthController]
 })
