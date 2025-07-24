060b944dfce931a7b1fca497bd4a8b8600bf896f
小熊幸汰 <siw23010010@gmail.com>
2025-07-24 18:43:18 +0900

    eventのservice、controller、moduleをnest gで追加

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,10 +6,13 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { AuthService } from './auth/auth.service';
 import { AuthController } from './auth/auth.controller';
+import { EventService } from './event/event.service';
+import { EventController } from './event/event.controller';
+import { EventModule } from './event/event.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
-  controllers: [AppController, AuthController],
-  providers: [AppService, PrismaService, AuthService],
+  imports: [UserModule, AuthModule, EventModule],
+  controllers: [AppController, AuthController, EventController],
+  providers: [AppService, PrismaService, AuthService, EventService],
 })
 export class AppModule {}
