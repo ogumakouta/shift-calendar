e404a79a123a7b44fa080bc378a0053fe2f41edc
小熊幸汰 <siw23010010@gmail.com>
2025-07-24 18:46:48 +0900

    必要なものをインポート コード追加：event.module.ts

--- backend/src/event/event.module.ts
+++ backend/src/event/event.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { EventController } from './event.controller';
+import { EventService } from './event.service';
+import { PrismaService } from '../prisma/prisma.service';
 
-@Module({})
+@Module({
+  controllers: [EventController],
+  providers: [EventService, PrismaService],
+})
 export class EventModule {}
