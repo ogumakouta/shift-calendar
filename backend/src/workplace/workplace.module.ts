61d2f8d62815e6bb373d2aff4b1b6c05fc179736
小熊幸汰 <siw23010010@gmail.com>
2025-07-25 15:26:55 +0900

    必要なものをインポート コード追加：workplace.module.ts

--- backend/src/workplace/workplace.module.ts
+++ backend/src/workplace/workplace.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { WorkplaceService } from './workplace.service';
+import { WorkplaceController } from './workplace.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
-@Module({})
+@Module({
+  controllers: [WorkplaceController],
+  providers: [WorkplaceService, PrismaService]
+})
 export class WorkplaceModule {}
