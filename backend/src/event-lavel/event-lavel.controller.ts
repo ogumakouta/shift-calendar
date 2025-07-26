9c0b22cfb1b3777ff94ff62f8da6f6e2f2b315fd
小熊幸汰 <siw23010010@gmail.com>
2025-07-26 21:37:30 +0900

    予定ラベルを取得するapiのルーティングを実装 コード追加：event-lavel.controller.ts

--- backend/src/event-lavel/event-lavel.controller.ts
+++ backend/src/event-lavel/event-lavel.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, ParseIntPipe, Get, Param } from '@nestjs/common';
+import { EventLavelService } from './event-lavel.service';
 
 @Controller('event-lavel')
-export class EventLavelController {}
+export class EventLavelController {
+  constructor(private readonly eventLavelService: EventLavelService) {}
+
+  @Get('/getLabels/:userId')
+  getLabels (
+    @Param('userId', ParseIntPipe) userId: number
+  ) {
+    return this.eventLavelService.getLabels(userId)
+  }
+}
