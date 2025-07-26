5d81321ea6be1fd2359b55491354ef808b3180db
小熊幸汰 <siw23010010@gmail.com>
2025-07-26 21:37:54 +0900

    必要なものをインポート コード追加：event-lavel.module.ts

--- backend/src/event-lavel/event-lavel.module.ts
+++ backend/src/event-lavel/event-lavel.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { EventLavelService } from './event-lavel.service';
+import { EventLavelController } from './event-lavel.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
-@Module({})
+@Module({
+  controllers: [EventLavelController],
+  providers: [EventLavelService, PrismaService],
+})
 export class EventLavelModule {}
