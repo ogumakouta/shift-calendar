2a42f33f387c05a310442c51614e6a230a5ed7cb
小熊幸汰 <siw23010010@gmail.com>
2025-07-26 21:38:35 +0900

    予定ラベルを取得する機能を実装 コード追加：event-lavel.service.ts

--- backend/src/event-lavel/event-lavel.service.ts
+++ backend/src/event-lavel/event-lavel.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class EventLavelService {}
+export class EventLavelService {
+  constructor(private prisma: PrismaService) {}
+
+  // 予定のラベルの取得
+  async getLabels(user_id: number) {
+    return this.prisma.eventLabel.findMany({
+      where: { user_id: user_id },
+    });
+  }
+}
