4cf115d4c4aa2b97cadf98b6ae7f598a67c1c390
小熊幸汰 <siw23010010@gmail.com>
2025-07-31 02:33:01 +0900

    ユーザのログイン情報をログインしてる時だけログアウトボタンを表示 コード追加：GetUser.tsx

---
+++ frontend/components/GetUser.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useRouter, usePathname } from 'next/navigation';
+
+
+export default function LogoutButton() {
+  const router = useRouter();
+  const [isLogin, setIsLogin] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    // LocalStorageからアクセストークンを取得
+    const token = localStorage.getItem('access_token');
+    // 取得できたらtrueにする
+    if (token) {
+      setIsLogin(true);
+    }
+  }, [pathname]);// URLが変更されるたびに実行
+
+  // LocalStorageからトークンを削除
+  function logout() {
+    localStorage.removeItem('access_token');
+    setIsLogin(false);
+    router.push('/login');
+  }
+
+  // ログインしてたら表示
+  if(isLogin) {
+    return <button className="p-3 bg-white border-glay-500 cursor-pointer rounded-md" onClick={logout}>ログアウト</button>
+  }
+
+  // ログインしてなかったら非表示
+  return null;
+}
