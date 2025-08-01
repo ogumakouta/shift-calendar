6c8ffb8742b897f0d7a02d0580e7bd1dc2f6e47d
小熊幸汰 <siw23010010@gmail.com>
2025-08-02 03:28:02 +0900

    親コンポーネントに値を共有 コード追加：Calendar.tsx

--- frontend/components/Calendar.tsx
+++ frontend/components/Calendar.tsx
@@ -1,31 +1,25 @@
 // 日付の選択（状態管理）やクリック操作を扱うため、クライアントコンポーネントにします。
 'use client';
 
-import { useState } from 'react';
 import Calendar from 'react-calendar';
-// デフォルトのスタイルをインポートします。
 import 'react-calendar/dist/Calendar.css';
 import  './Calendar.css';
+import type { Value } from "react-calendar/dist/cjs/shared/types";
 
-// react-calendarが提供する型をインポート
-type ValuePiece = Date | null;
-type Value = ValuePiece | [ValuePiece, ValuePiece];
 
-export default function MyCalendar() {
-  // 選択された日付を保持するためのstate
-  const [date, setDate] = useState<Value>(new Date());
+// 親コンポーネントから受け取るpropsの型定義
+type Props = {
+  value: Value;
+  onChange: (value: Value) => void;
+};
+
+export default function MyCalendar({ value, onChange}: Props) {
 
   return (
-    <div className="flex">
-      <div className="text-center mt-4">
-        {/* Dateオブジェクトを文字列に変換して表示 */}
-        {/* <p>選択中の日付: {date instanceof Date ? date.toLocaleDateString('ja-JP') : '日付を選択してください'}</p> */}
-      </div>
-      <Calendar 
-        onChange={setDate} 
-        value={date} 
-        locale="ja-JP"
-      />
-    </div>
+    <Calendar 
+      onChange={onChange}
+      value={value} 
+      locale="ja-JP"
+    />
   );
 }
