222e0bc69378b06dc0a10b0da01f063c7ea8d9de
小熊幸汰 <siw23010010@gmail.com>
2025-08-06 01:04:27 +0900

    CheckLoginを追加 コード追加：login/page.tsx

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,9 +1,12 @@
 'use client'
 import LoginForm from "../../../components/LoginForm";
+import CheckLogin from '../../../components/CheckLogin';
+
 
 export default function LoginPage() {
   return (
     <main>
+      <CheckLogin/>
       <h1 style={{ textAlign: 'center' }}>ログイン</h1>
       <LoginForm />
     </main>
