d696d5cbda84acdc98e2ecffb39b3661f43c730f
小熊幸汰 <siw23010010@gmail.com>
2025-08-12 18:09:57 +0900

    設定画面で設定ボタンを非表示にするロジックを追加 コード追加：SettingPageButton.tsx

--- frontend/components/SettingPageButton.tsx
+++ frontend/components/SettingPageButton.tsx
@@ -2,9 +2,14 @@
 import Image from "next/image";
 import settingBtn from '../public/setting_icon.svg';
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function SettingPageButton() {
-  return (
-    <Link href='/setting' className='m-2'><Image src={settingBtn} alt='設定' height={48}/></Link>
-  );
+  const pathname = usePathname();
+
+  if (pathname !== '/setting') {
+    return <Link href='/setting' className='m-2'><Image src={settingBtn} alt='設定' height={48}/></Link>
+  }
+
+  return null;
 }
