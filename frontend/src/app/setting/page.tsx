3e2960fb9df31f8bf8be06b2b3e46900416b9ae6
小熊幸汰 <siw23010010@gmail.com>
2025-08-14 02:28:15 +0900

    ユーザIDを取得して子コンポーネントに渡すロジックを追加 コード追加：setting/page.tsx

--- frontend/src/app/setting/page.tsx
+++ frontend/src/app/setting/page.tsx
@@ -2,12 +2,29 @@
 
 import UserSettingForm from "../../../components/UserSettingForm";
 import EventLabelSettingForm from "../../../components/EventLabelSettingForm";
+import { useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
 
 export default function SettingPage() {
+  const [user_id, setUser_id] = useState('');
+
+  // ユーザIDを取得
+  useEffect(() => {
+    const token = localStorage.getItem('access_token');
+    if (!token) {
+      console.log('ユーザIDが見つかりません')
+      return;
+    }
+
+    const decodeToken = jwtDecode(token);
+    setUser_id(decodeToken.sub)
+    console.log('ユーザID取得')
+  }, [])
+
   return (
     <div>
       <UserSettingForm/>
-      <EventLabelSettingForm/>
+      <EventLabelSettingForm user_id={user_id} />
     </div>
   );
 }
