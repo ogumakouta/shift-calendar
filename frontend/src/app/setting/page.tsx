7b03dfac4b2edfe50e05293067c25899bab26171
小熊幸汰 <siw23010010@gmail.com>
2025-08-14 00:27:19 +0900

    EventLabelSettingFormをインポートして表示 コード追加：setting/page.tsx

--- frontend/src/app/setting/page.tsx
+++ frontend/src/app/setting/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 
 import UserSettingForm from "../../../components/UserSettingForm";
+import EventLabelSettingForm from "../../../components/EventLabelSettingForm";
 
 export default function SettingPage() {
   return (
     <div>
       <UserSettingForm/>
+      <EventLabelSettingForm/>
     </div>
   );
 }
