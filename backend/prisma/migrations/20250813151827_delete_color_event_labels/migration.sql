ba5b0bdb0b0704a2c0a1ea1f16de17b5ba3c0d1c
小熊幸汰 <siw23010010@gmail.com>
2025-08-14 00:18:55 +0900

    event_labelsテーブルのcolorカラムを削除

---
+++ backend/prisma/migrations/20250813151827_delete_color_event_labels/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `color` on the `event_labels` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `event_labels` DROP COLUMN `color`;
