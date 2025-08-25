7aa392a4588c8862a6cb077ab5ab75d97d41a5bb
小熊幸汰 <siw23010010@gmail.com>
2025-08-25 09:38:42 +0900

    ホバーした時のダークモード用のスタイルを追加 コード追加：CalendarPageButton.tsx

--- frontend/components/CalendarPageButton.tsx
+++ frontend/components/CalendarPageButton.tsx
@@ -6,7 +6,7 @@ export function CalendarPageButton() {
   return (
     <Link href='calendar' 
       className="flex justify-center text-center p-2 bg-[#fbfbfb] rounded-md hover:bg-gray-200 transition-colors
-      dark:bg-[#565656]"
+      dark:bg-[#565656] dark:hover:bg-gray-500"
     >
       カレンダーページ<br></br>に戻る
     </Link>
